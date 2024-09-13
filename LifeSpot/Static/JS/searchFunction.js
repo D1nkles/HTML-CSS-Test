2dbc05911720cddf175c6337e2450123d28a4fea
Dinkles <isinetov@mail.ru>
2024-09-13 16:24:52 +0400

    Задание 29.4.4

--- LifeSpot/Static/JS/searchFunction.js
+++ LifeSpot/Static/JS/searchFunction.js
@@ -1,10 +1,10 @@
-﻿function searchFunction(userInput)
+﻿function searchFunction(getInputFunction)
 {
     let videoElements = document.getElementsByClassName('video-container');
     for (let i = 0; i <= videoElements.length; i++) {
     let childElements = videoElements[i];
-    let videoDescription = childElements.getElementsByTagName('h3')[0].innerText.toLowerCase();
-    if (videoDescription.includes(userInput)) {
+        let videoDescription = childElements.getElementsByTagName('h3')[0].innerText.toLowerCase();
+        if (videoDescription.includes(getInputFunction())) {
             videoElements[i].style.display = 'inline-block';
     }
     else {
